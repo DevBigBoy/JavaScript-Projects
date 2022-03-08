10630559dbcce7191b9de388b432d36234758ec1
DevMohamedElshazly <moshazly416@gmail.com>
2022-03-08 09:46:57 +0200

    Add-Project

--- 003-Popup-Modal/js/app.js
+++
@@ -1,9 +0,0 @@
-const number = document.querySelector(".number");
-const btn = document.querySelector(".btn");
-
-let generatNumber = () => {
-  const randomN = Math.floor(Math.random() * 100);
-  number.innerHTML = randomN;
-};
-
-btn.addEventListener("click", generatNumber);
