1afd30152aa617383718d91f3c7f4a71a85186f7
DevMohamedElshazly <moshazly416@gmail.com>
2022-04-16 14:04:34 +0200

    add

--- 013-Notes-App/main.js
+++ 013-Notes-App/main.js
@@ -6,17 +6,86 @@ const addBox = document.querySelector(".add-box"),
   descTag = popupBox.querySelector("textarea"),
   addBtn = popupBox.querySelector("button");
 
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+//? Getting Localstorage Notes if Exist and Parsing Them
+//? to js Object else passing an empty array to notes
+
+const notes = JSON.parse(localStorage.getItem("notes") || "[]");
+
 addBox.addEventListener("click", () => {
   popupBox.classList.add("show");
 });
 
 closeIcon.addEventListener("click", () => {
+  //* Make these inputs blank when user close popup
+  titleTag.value = "";
+  descTag.value = "";
   popupBox.classList.remove("show");
 });
 
+function showNotes() {
+  //* Removing All Previous notes before adding new
+  document.querySelectorAll(".note").forEach((note) => note.remove());
+  notes.forEach((note) => {
+    let liTag = `
+     <li class="note">
+         <div class="details">
+            <p> ${note.title}</p>
+            <span>${note.description}</span>
+        </div>
+        <div class="bottom-content">
+            <span>${note.date}</span>
+            <div class="settings">
+                <i class="uil uil-ellipsis-h"></i>
+                <ul class="menu">
+                    <li><i class="uil uil-pen"></i> Edite</li>
+
+                    <li><i class="uil uil-trash"></i> Delete</li>
+                </ul>
+            </div>
+        </div>
+    </li>`;
+
+    addBox.insertAdjacentHTML("afterend", liTag);
+  });
+}
+
+showNotes();
+
 addBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  let title = titleTag.value.trim(),
-    description = descTag.value.trim();
-  console.log(title, description);
+  let noteTitle = titleTag.value.trim(),
+    noteDesc = descTag.value.trim();
+
+  if (noteTitle || noteDesc) {
+    let dateObj = new Date(),
+      month = months[dateObj.getMonth()],
+      day = dateObj.getDate(),
+      year = dateObj.getFullYear();
+
+    let noteInfo = {
+      title: noteTitle,
+      description: noteDesc,
+      date: `${month} ${day}, ${year}`,
+    };
+
+    notes.push(noteInfo); // adding new note to notes
+    localStorage.setItem("notes", JSON.stringify(notes));
+
+    closeIcon.click();
+    showNotes();
+  }
 });
